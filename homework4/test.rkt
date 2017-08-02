1df4ae0215a722a70aee7805e3c7f43f8adf7c9b
jsyishan <404678173@qq.com>
2017-08-02 21:33:55 +0800

    First assignment for Racket(hw4) finished, graded 105.0%

--- homework4/test.rkt
+++ homework4/test.rkt
@@ -19,13 +19,36 @@
 
 (define powers-of-two
     (letrec ([f (lambda (x) (cons x (lambda () (f (* x 2)))))])
-        (lambda () (f 2)))) ; return a function, which contains(when it's called) a pair
-                            ;'(value, promise(the uncalled function containing the next pair))
+        (lambda () (f 2)))) ; return a function(thunk), which contains(when it's called) a pair
+                            ;'(value, promise(the uncalled function(thunk) containing the next pair))
 
 (define (number-until stream tester)
     (letrec ([f (lambda (stream ans)
-        (let ([pr (stream)])    ; the pair returned from calling function 'stream'
+        (let ([pr (stream)])    ; the pair returned from calling thunk 'stream'
             (if (tester (car pr))   ; the value of the stream (stored at the pair 'pr')
                 ans
                 (f (cdr pr) (+ ans 1)))))])
     (f stream 1)))  ; tail recursion
+
+
+(define (fibonacci x)
+    (letrec ([f (lambda (acc1 acc2 y)
+        (if (= y x)
+        (+ acc1 acc2)
+        (f (+ acc1 acc2) acc1 (+ y 1))))])
+    (if (or (= x 1)(= x 2))
+        1
+        (f 1 1 3))))
+
+(define fibonacci2
+    (letrec([memo null]
+            [f (lambda (x)
+                (let ([ans (assoc x memo)])
+                    (if ans
+                        (cdr ans)
+                        (let ([new-ans (if (or (= x 1) (= x 2))
+                                        1
+                                        (+ (f (- x 1))(f (- x 2))))])
+                        (begin(set! memo (cons (cons x new-ans) memo))
+                               new-ans)))))])
+    f))
